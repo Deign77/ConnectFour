2d8832046498199cc5282878a7543651d07e955b
Deign77 <david.wesseling1@gmail.com>
2020-03-31 10:22:31 +0100

    New branch to experiment with only having the option to play against the computer, no human opponent

--- ConnectFour/Form1.cs
+++ ConnectFour/Form1.cs
@@ -12,9 +12,11 @@ namespace ConnectFour
 {
     public partial class ConnectFour : Form
     {
-        private bool playerColourRed = true;
+        public bool playerTurn = true;
 
-        private static bool playerTurn = true;
+        public bool computerPlayer = false;
+
+        public bool computerMoveFirst = false;
 
         public static int playerScore = 0;
         public static int computerScore = 0;
@@ -48,6 +50,11 @@ namespace ConnectFour
             
         }
 
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            startNewGame();
+        }
+
         //runs on start, intializes board
         private void InitializeBoard()
         {
@@ -60,9 +67,6 @@ namespace ConnectFour
             }
 
             scoreBoard = new int[8, 10];
-
-            P1Score.Text = playerScore.ToString();
-            P2Score.Text = computerScore.ToString();
 
             //setting dimensions of the columns and rows
             foreach (DataGridViewColumn c in gameBoard.Columns)
@@ -85,41 +89,51 @@ namespace ConnectFour
             }
         }
 
-        //ask player to choose their colour 
-        private static bool chooseColour()
-        {
-            DialogResult d = MessageBox.Show("Do you want to be red?", "Colour Choice", MessageBoxButtons.YesNo);
-
-            if (d == DialogResult.Yes) MessageBox.Show("Okay, your colour is red!", "Red");
-            else MessageBox.Show("Fine, you can be yellow and I'll be red!", "Yellow");
-
-            return (d == DialogResult.Yes) ? true : false;
-        }
-
         //ask player to choose who gets first move
         private static bool chooseFirstMove()
         {
             DialogResult d = MessageBox.Show("Do you want to have the first turn?", "Who goes first?", MessageBoxButtons.YesNo);
-            if (d == DialogResult.Yes)
-            {
-                MessageBox.Show("Okay, you get to move first.", "Player turn first");
-                playerTurn = true;
-            }   
-            else
-            {
-                MessageBox.Show("Okay, I'll go first.", "Computer turn first");
-                playerTurn = false;
-            }
+
+            if (d == DialogResult.Yes) MessageBox.Show("Okay, you get to move first.", "Player turn first");
+
+            else MessageBox.Show("Okay, I'll go first.", "Computer turn first");
             
             return d == DialogResult.Yes ? true : false;
         }
+        //ask player to choose their colour (first game only)
+        private void chooseColour()
+        {
+            DialogResult d = MessageBox.Show("Do you want to be red?", "Colour Choice", MessageBoxButtons.YesNo);
+
+            if (d == DialogResult.Yes) MessageBox.Show("Okay, your colour is red!", "Red");
+
+            else MessageBox.Show("Fine, you can be yellow and I'll be red!", "Yellow");
+
+            P1ScoreLabel.BackColor = d == DialogResult.Yes ? Color.Red : Color.Yellow;
+            P2ScoreLabel.BackColor = d == DialogResult.Yes ? Color.Yellow : Color.Red;
+
+            P1ScoreLabel.ForeColor = P2ScoreLabel.ForeColor = Color.Black;
+        }
+
         //toggle colour & text of player turn button
-        private void turnButton(bool colour, bool turn)
+        private void turnLabel(bool turn)
         {
             textBox1.Text = turn ? "Player's turn!" : "Computer's turn!";
+            textBox1.BackColor = turn ? P1ScoreLabel.BackColor : P2ScoreLabel.BackColor;
+        }
+        //ask if player wants to play against the computer
+        private void playVsComp()
+        {
+            DialogResult d = MessageBox.Show("Do you want to play against the computer?", "Play against computer?", MessageBoxButtons.YesNo);
 
-            if (turn) textBox1.BackColor = colour ? Color.Red : Color.Yellow;
-            else textBox1.BackColor = !colour ? Color.Red : Color.Yellow;
+            if (d == DialogResult.Yes)
+            {
+                MessageBox.Show("Okay, I'll play against you!", "Computer");
+                computerPlayer = true;
+                P2ScoreLabel.Text = "Computer Score:";
+            }
+
+            else MessageBox.Show("Okay, you can just play with yourself!", "Play with yourself");
         }
 
         //starts a new game
@@ -127,27 +141,69 @@ namespace ConnectFour
         {
             InitializeBoard();
 
-            playerColourRed = chooseColour();
+            
 
-            playerTurn = chooseFirstMove();
+            turnLabel(playerTurn);
 
-            turnButton(playerColourRed, playerTurn);
+            if (P1ScoreLabel.BackColor != Color.Blue)
+            {
+                 if (!computerPlayer) playerTurn = !playerTurn;
+                 turnLabel(playerTurn);
+            }
+            else
+            {
+                chooseColour();
+                playVsComp();
+            }
+
+
+
+            playerTurn = computerMoveFirst = chooseFirstMove();
+
+            
+
+            //if (playerTurn == false && computerPlayer) computerMoveFirst = true;
+
+            turnLabel(playerTurn);
+            /*
+            if (!playerTurn && computerPlayer && computerMoveFirst)
+            {
+                computerTurnRandom();
+                computerMoveFirst = false;
+            }
+            */
         }
 
-        //  NEW GAME CLICK
-        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        //program to take a (randomly placed) turn for the computer
+        private void computerTurnRandom()
         {
-            startNewGame();
+            //wait for 1 seconds before taking turn
+            System.Threading.Thread.Sleep(1000);
+
+            Random ran = new Random();
+
+            int colIndex = ran.Next(0, gameBoard.ColumnCount - 1);
+
+            int rowInd = gameBoard.RowCount - 1;
+
+            while (scoreBoard[rowInd, colIndex] != 0)
+            {
+                //warning: don't let computer fill up a whole column
+                rowInd--;
+            }
+            gameBoard[colIndex, rowInd].Style.BackColor = P2ScoreLabel.BackColor;
+
+            scoreBoard[rowInd, colIndex] = 2;
+            winChecker(rowInd, colIndex, 2);
+            playerTurn = true;
         }
+
+
 
         //GAMEBOARD CLICK
         private void gameBoard_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
             int rowIndex = gameBoard.RowCount - 1;
-
-            
-
 
             while (scoreBoard[rowIndex, e.ColumnIndex] != 0)
             {
@@ -159,21 +215,14 @@ namespace ConnectFour
                 rowIndex--;
             }
 
-            
-            //gameBoard[e.ColumnIndex, rowIndex].Style.BackColor = textBox1.BackColor == Color.Yellow ? Color.Yellow : Color.Red;
-
-
-            turnButton(playerColourRed, !playerTurn);
-            
             if (playerTurn)
             {
-                gameBoard[e.ColumnIndex, rowIndex].Style.BackColor = playerColourRed ? Color.Red : Color.Yellow;
+                gameBoard[e.ColumnIndex, rowIndex].Style.BackColor = P1ScoreLabel.BackColor;
             }
-            else
+            else if (!computerPlayer)
             {
-                gameBoard[e.ColumnIndex, rowIndex].Style.BackColor = playerColourRed ? Color.Yellow : Color.Red;
+                gameBoard[e.ColumnIndex, rowIndex].Style.BackColor = P2ScoreLabel.BackColor;
             }
-           
 
             int lastTurn = playerTurn ? 1 : 2;
 
@@ -181,46 +230,59 @@ namespace ConnectFour
             scoreBoard[rowIndex, e.ColumnIndex] = lastTurn;
 
 
+            winChecker(rowIndex, e.ColumnIndex, lastTurn);
 
-            if (
-                //checks left and right
-                checkOpDir(rowIndex, e.ColumnIndex, lastTurn, new int[] { 0, -1, 0, 1 }, false) || 
+            //playerTurn = !playerTurn;
+
+
+
+            //if computer is playing
+            if (computerPlayer == true && playerTurn == false)
+            {
+                turnLabel(playerTurn);
+                computerTurnRandom();    
+            }
+
+            turnLabel(playerTurn);
+            
+        }
+        //checks if the last turn won the game and if true, ends the game
+        public void winChecker(int ri, int ci, int turn)
+        {
+            if (//checks left and right
+                checkBothWays(ri, ci, turn, new int[] { 0, -1, 0, 1 }, false) ||
                 //checks up and down
-                checkOpDir(rowIndex, e.ColumnIndex, lastTurn, new int[]{-1, 0, 1, 0}, true)  ||
+                checkBothWays(ri, ci, turn, new int[] { -1, 0, 1, 0 }, true) ||
                 //check upleft and downright
-                checkOpDir(rowIndex, e.ColumnIndex, lastTurn, new int[] { -1, -1, 1, 1 }, true) || 
+                checkBothWays(ri, ci, turn, new int[] { -1, -1, 1, 1 }, true) ||
                 //check upright and downleft
-                checkOpDir(rowIndex, e.ColumnIndex, lastTurn, new int[] { -1, 1, 1, -1 }, true) 
-                )
+                checkBothWays(ri, ci, turn, new int[] { -1, 1, 1, -1 }, true))
             {
                 if (playerTurn)
                 {
                     MessageBox.Show("You win!", "Winner");
                     playerScore++;
-                }     
+                    P1Score.Text = playerScore.ToString();
+                }
                 else
                 {
                     MessageBox.Show("You lose!", "Loser");
                     computerScore++;
+                    P2Score.Text = computerScore.ToString();
                 }
 
-
-                
-
+                computerMoveFirst = false;
                 DialogResult q = MessageBox.Show("Do you want to play again?", "Play again?", MessageBoxButtons.YesNo);
                 if (q == DialogResult.Yes) startNewGame();
                 else Close();
             }
-
-            
-            //important
-            playerTurn = !playerTurn;
         }
        
-        //winChecker
-        public bool checkOpDir(int r, int c, int lastTurn, int[] ar, bool isRow)
+        //part of win checker
+        public bool checkBothWays(int r, int c, int lastTurn, int[] ar, bool isRow)
         {
             int numsInRow = 1;
+            //var winNums = new List<int>();   for making blinking winning blocks
             //check this way
             try
             {
